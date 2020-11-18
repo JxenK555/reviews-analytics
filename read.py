99ecf7092af0af1f9853e8203df373e67c7a9317
Jxen Khor <jxenkhor@gmail.com>
2020-11-18 15:43:29 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -6,8 +6,9 @@ with open('reviews.txt', 'r')as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('All has been read, Total', len(data), 'comment')
 
-print(data[0])
-print('----------------------------------------------------------------------------------------------------------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('All comment average length', sum_len/len(data))
