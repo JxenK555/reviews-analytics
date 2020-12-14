d7640822c4af7332d07cb4dde1c90f5e28deba20
Jxen Khor <jxenkhor@gmail.com>
2020-12-14 17:50:36 +0800

    Add word count function

--- read.py
+++ read.py
@@ -1,3 +1,26 @@
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('All comment average length', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('Have', len(new), 'comment less than 100 words')
+print(new[0])
+print(new[1])
+
+good = [d for d in data if 'good' in d]
+
+good = []
+for d in data:
+	if 'good'in d:
+		good.append(d)
+print('Have', len(good), 'comment is a good comment')
+print(good[0])
+
+
 data = []
 count = 0
 with open('reviews.txt', 'r')as f:
@@ -6,18 +29,32 @@ with open('reviews.txt', 'r')as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print('All has been read, Total', len(data), 'comment')
+print('All has been rad, Total', len(data), 'comment')
 
-sum_len = 0
+print(data[0])
+
+#文字技術
+wc = {}  #wordcount
 for d in data:
-	sum_len = sum_len + len(d)
-print('All comment average length', sum_len/len(data))
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1  #有等於就是新加key
 
-new = []
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
 
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('Have', len(new), 'comment less than 100 words')
-print(new[0])
-print(new[1])
+print(len(wc))
+
+while True:
+	word = input('What do u want to search: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, 'came out', wc[word], 'time')
+	else:
+		print('I dont get this word, Try another one.')
+print('Thanks For Your Using')
